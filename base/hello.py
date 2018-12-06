2d671dec0c3a925a47037cd65eb06a2070f3ddf4
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2018-12-06 18:38:49 +0800

    add:project struct

---
+++ base/hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'a hello module'
+__author__ = 'jxp'
+__doc__='study and test'
+# import mysql.connector
+#name = input('input your name:')
+#print('welcome:',name)
+n1=10
+
+print(n1/3)
+print(r'''hello,\n world''')
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
+
+import sys
+args = sys.argv
+print(args)
