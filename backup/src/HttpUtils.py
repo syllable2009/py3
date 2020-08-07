91acbfc859564269260912c5c0ca1cd075744e94
syllable2009 <424772147@qq.com>
2020-08-08 01:04:40 +0800

    modify
    123

--- backup/src/HttpUtils.py
+++ backup/src/HttpUtils.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     # http.parseResult(http.myParse)
     # HttpUtils.download("",
     #                    "/Users/jiaxiaopeng/d.png")
-    url = 'https://message.corp.kuaishou.com/api/file/preview/ccfb7c2c-a30e-4baa-b828-59e357a3b0a5?fileSuffix=png';
+    url = 'https://';
     r = requests.get(url,headers=HttpUtils.headers2)
     with open("/Users/jiaxiaopeng/e.png", "wb") as code:
         code.write(r.content)
