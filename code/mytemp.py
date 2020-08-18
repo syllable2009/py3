a646dbe00859bcec7ad734b26200dec2a322e5cc
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2020-08-18 18:38:49 +0800

    update:modify save

--- code/mytemp.py
+++ code/mytemp.py
@@ -1,10 +1,13 @@
-import time
-def decorator(func):
-    def punch():
-        print(time.strftime('%Y-%m-%d', time.localtime(time.time())))
-        func()
-    return punch
-def punch():
-    print('昵称：两点水  部门：做鸭事业部 上班打卡成功')
-f = decorator(punch)
-f()
+# -*- coding: UTF-8 -*-
+def odd():
+    print ( 'step 1' )
+    yield ( 1 )
+    print ( 'step 2' )
+    yield ( 3 )
+    print ( 'step 3' )
+    yield ( 5 )
+
+o = odd()
+print( next( o ) )
+print( next( o ) )
+print( next( o ) )
